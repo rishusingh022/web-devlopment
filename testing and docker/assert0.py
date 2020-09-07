40a914a160f648c00cbf84625d01839068123599
rishusingh022 <rishusingh022@gmail.com>
2020-09-07 19:06:58 +0530

    added security scalibility and docker

---
+++ testing and docker/assert0.py
@@ -0,0 +1,6 @@
+def square(x):
+    return x * x
+#check whether its correct or not
+print(square(10)==100)
+#if our code is correct no output if not it will through exception just testing the code is correct or not
+assert square(10) == 100
